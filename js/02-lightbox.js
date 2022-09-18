c4033675ca896605e3e646435cf677ab0f158619
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-18 18:36:07 +0200

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+/** @format */
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryImages = document.querySelector(".gallery");
+const itemMarkup = createGalleryImage(galleryItems);
+
+galleryImages.innerHTML = itemMarkup;
+
+function createGalleryImage(img) {
+  return img
+    .map(({ preview, description, original }) => {
+      return ` <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
