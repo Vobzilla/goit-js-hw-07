112ce327a5b5845556ceac9fcb0f29c3be641d59
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-18 16:26:18 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+/** @format */
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryImages = document.querySelector(".gallery");
+const itemMarkup = createGalleryImage(galleryItems);
+
+galleryImages.insertAdjacentHTML("beforeend", itemMarkup);
+galleryImages.addEventListener("click", onOpenGalleryImages);
+
+function createGalleryImage(img) {
+  return img
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onOpenGalleryImages(event) {
+  event.preventDefault();
+
+  const isImagesSwatchEl = event.target.classList.contains("gallery__image");
+  if (!isImagesSwatchEl) {
+    return;
+  }
+
+  const currentImg = event.target.attributes;
+
+  const instance = basicLightbox.create(
+    `<div><img src='${currentImg[2].value}' alt='${currentImg.alt.nodeValue}'></div>`
+  );
+  instance.show();
+}
